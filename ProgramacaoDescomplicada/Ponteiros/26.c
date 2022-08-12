8ae067a6600e01602fd55611ae84eb7353f89d45
Rafael Ferrao <rafa5ferrao@gmail.com>
2022-08-12 19:14:56 -0300

    updating codes

--- ProgramacaoDescomplicada/Ponteiros/26.c
+++ ProgramacaoDescomplicada/Ponteiros/26.c
@@ -1,27 +1,31 @@
 // Implemente uma func¸ao que calcule as ra ˜ ´ızes de uma equac¸ao do segundo grau do tipo ˜
 // Ax2 + Bx + C = 0. BLA BLA BLA
-//Essa func¸ao deve obedecer ao seguinte prot ˜ otipo: ´
+// Essa func¸ao deve obedecer ao seguinte prot ˜ otipo: ´
 // int raizes(float A,float B,float C,float * X1,float * X2);
 
 #include <stdio.h>
 #include <math.h>
 
-int raizes(float A,float B,float C,float *X1,float *X2){
-    float delta = -4 * A * C + (B*B);
+int raizes(float A, float B, float C, float *X1, float *X2)
+{
+    float delta = -4 * A * C + (B * B);
     printf("%f", delta);
 
-    if(delta < 0)
+    if (delta < 0)
         return 0;
-    else if(delta == 0){
-        *X1 = (-B + sqrt(delta))/(2 * A);
+    else if (delta == 0)
+    {
+        *X1 = (-B + sqrt(delta)) / (2 * A);
         *X2 = *X1;
-        return 1;}
-    else{
-        *X1 = (-B + sqrt(delta))/(2 * A);
-        *X2 = (-B - sqrt(delta))/(2 * A);
+        return 1;
+    }
+    else
+    {
+        *X1 = (-B + sqrt(delta)) / (2 * A);
+        *X2 = (-B - sqrt(delta)) / (2 * A);
 
         return 2;
-        }
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -40,9 +44,5 @@ int main(int argc, char const *argv[])
     printf("\n X': %f", X1);
     printf("\n X'': %f", X2);
 
-
-
-
-    
     return 0;
 }
--- Semana_01/Lista_de_exercicios_01/exerc2.c
+++ Semana_01/Lista_de_exercicios_01/exerc2.c
@@ -6,89 +6,72 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void AddName(int *nameListCharAmount, char *nameList);
-void ListNames(int *nameListCharAmount, char *nameList);
+char *AddName(char *agenda, int *size) {
+    char c = '\0';
+    do {
+        (*size)++;
+        c = getchar();
+        if (c != '\n') {
+            agenda = (char *)realloc(agenda, sizeof(char) * (*size));
+            agenda[(*size) - 1] = c;
+        } else {
+            agenda[(*size) - 1] = '\0';
+        }
+    } while (c != '\n');
+    
+    return agenda;
+}
 
-int main()
-{
-    char *nameList;
+void ListAgenda(char *agenda, int *size){
+        for (int i = 0; i < (*size) - 1; i++) {
+            if(agenda[i] == '\0' || i == 0)
+                printf("\nNome: ");
+        printf("%c", agenda[i]);
+    }
+    printf("\n");
+}
 
-    nameList = (char *)malloc(sizeof(char) * 1);
-    if (!nameList)
-        printf("Erro! Falta de memoria");
-    int nameListCharAmount = 0;
-    int namesAmount = 0;
+int main() {
 
-    AddName(&nameListCharAmount, nameList);
-    AddName(&nameListCharAmount, nameList);
+    char *agenda = (char *)malloc(sizeof(char));
+    int agendaSize = 0;
+    int a = 0;
 
-    ListNames(&nameListCharAmount, nameList);
+    int userChoice = 0;
+    
+    do
+    {
+        printf("\n-----Faca sua escolha-----\n1) Adicionar nome\n2) Remover nome\n3) Listar nome\n4) Parar\nSua escolha: ");
 
+        scanf("%d", &userChoice);
+        fgetc(stdin);
 
-    // int a = 0;
-    // char userChoice;
-    // do
-    // {
+        switch (userChoice)
+        {
+        case 1:
+            printf("\nInsira o nome: ");
+            agenda = AddName(agenda, &agendaSize);
+            break;
 
-    //     printf("----Insira a opcao desejada----\n1) Adicionar nome\n2) Remover nome\n3) Listar\n4) Sair\n\nSua escolha:");
-    //     scanf("%c", &userChoice);
-    //     switch (userChoice)
-    //     {
-    //     case '1':
-    //         printf("%c", userChoice);
-    //         break;
-    //     case '2':
-    //         break;
-    //     case '3':
-    //         ListNames();
-    //         break;
-    //     case '4':
-    //         break;
-    //     }
-    // } while (userChoice != '4');
+        case 3:
+            ListAgenda(agenda, &agendaSize);
+            break;
+
+        case 4:
+            break;
+        
+        default:
+            printf("\nEntrada invalida! Insira novamente.");
+            break;
+        }
+
+    } while (userChoice != 4);
+    
+
+        
+    free(agenda);
 
     return 0;
-}
-
-/*
-====================================================================================================
-AddName
-This function reads a String input from the user and save it in a dynamic char array.
-During the input reading, the dynamic array will use realloc to fit itself to the user's input.
-====================================================================================================
-*/
-
-void AddName(int *nameListCharAmount, char *nameList)
-{
-
-    while ((nameList[(*nameListCharAmount)] = fgetc(stdin)) != '\n')
-    {
-        ++(*nameListCharAmount);
-        nameList = realloc(nameList, sizeof(char) * (*nameListCharAmount));
-        if (!nameList)
-            printf("Erro! Falta de memoria");
-    }
-
-    nameList[(*nameListCharAmount)] = '\0';
-    (*nameListCharAmount)++;
-}
-
-/*
-======================================================================================================
-ListNames
-This function prints the names in the array, one per line.
-It funcionality allows it to print char per char of the string until it finds the '\0' between the names, wich I use as separators.
-For each name, we have one separator.
-======================================================================================================
-*/
-
-void ListNames(int *nameListCharAmount, char *nameList)
-{
-    for (int i = 0; i < (*nameListCharAmount) - 1; i++)
-    {
-        if (nameList[i] == '\0' || i == 0)
-            printf("\nNome: ");
-        printf("%c", nameList[i]);
-    }
 }
