6e59a78ccf3c069db67ca6f742951de218e0f69f
RafaelFerrao <rafa5ferrao@gmail.com>
2022-08-22 22:59:00 -0300

    adaptado para o code style doom3

--- Semana_03/ex5.c
+++ Semana_03/ex5.c
@@ -2,86 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
-{
+typedef struct {
     char nome[30];
     int idade;
     int altura;
-} Pessoa;
-
-int main()
-{
+} pessoa_t;
+    
+int main() {
     char op = '1';
     int nPessoas = 0;
     char nome[30];
 
-    Pessoa *buffer = (Pessoa *)malloc(sizeof(Pessoa));
-    Pessoa *pBuffer;
+    pessoa_t *buffer = ( pessoa_t * )malloc( sizeof( pessoa_t ) );
+    const pessoa_t *pBuffer;
     char *ptrNome;
-    int *ptrIdade;
-    int *ptrAltura;
+    const int *ptrIdade;
+    const int *ptrAltura;
 
     
-    do{
-        printf("--------------------\nEscolha uma opcao\n1-Inserir nova pessoa\n0- Sair\nSua escolha: ");
+    do {
+        printf( "--------------------\nEscolha uma opcao\n1-Inserir nova pessoa\n0- Sair\nSua escolha: " );
 
-        setbuf(stdin, NULL);
+        setbuf( stdin, NULL );
         op = getchar();
 
-        switch (op){
+        switch ( op ){
         case '1':
             nPessoas++;
-            buffer = realloc(buffer, sizeof(Pessoa) * nPessoas);
+            buffer = realloc( buffer, sizeof( pessoa_t ) * nPessoas );
             pBuffer = buffer;
 
-            for(int i = 1; i < nPessoas; i++){
+            for(int i = 1; i < nPessoas; i++) {
                 pBuffer++;
             }
 
-            ptrNome = (char *)pBuffer;
-            ptrIdade = (int *)(pBuffer + sizeof(char)*30);
-            ptrAltura = (int *)(pBuffer + sizeof(char)*30 + sizeof(int));
+            ptrNome = ( char * )pBuffer;
+            ptrIdade = ( int * )( pBuffer + sizeof( char )*30 );
+            ptrAltura = ( int * )( pBuffer + sizeof( char )*30 + sizeof( int ) );
 
-            printf("\n--------------------\nInsira o nome da pessoa: ");
-            setbuf(stdin, NULL);
-            gets(nome);
-            strcpy(ptrNome, nome);
+            printf( "\n--------------------\nInsira o nome da pessoa: " );
+            setbuf( stdin, NULL );
+            gets( nome );
+            strcpy( ptrNome, nome );
 
-            printf("\nInsira a idade da pessoa: ");
-            setbuf(stdin, NULL);
-            scanf("%d", ptrIdade);
+            printf( "\nInsira a idade da pessoa: " );
+            setbuf( stdin, NULL );
+            scanf( "%d", ptrIdade );
 
-            printf("\nInsira a altura da pessoa: ");
-            setbuf(stdin, NULL);
-            scanf("%d", ptrAltura);
-            system("cls");
+            printf( "\nInsira a altura da pessoa: " );
+            setbuf( stdin, NULL );
+            scanf( "%d", ptrAltura );
+            system( "cls" );
             break;
 
         case '0':
             pBuffer = buffer;
-            for(int i = 0; i < nPessoas; i++){
-                
+            for( int i = 0; i < nPessoas; i++ ){
+            
+            ptrNome = ( char * )pBuffer;
+            ptrIdade = ( int * )( pBuffer + sizeof( char )* 30 );
+            ptrAltura = ( int * )( pBuffer + sizeof( char )*30 + sizeof( int ) );
 
-            ptrNome = (char *)pBuffer;
-            ptrIdade = (int *)(pBuffer + sizeof(char)*30);
-            ptrAltura = (int *)(pBuffer + sizeof(char)*30 + sizeof(int));
-
-            printf("\n--------------------");
-            printf("\nNome: %s", ptrNome);
-            printf("\nIdade: %d", *ptrIdade);
-            printf("\nAltura: %d", *ptrAltura);
-
+            printf( "\n--------------------" );
+            printf( "\nNome: %s", ptrNome );
+            printf( "\nIdade: %d", *ptrIdade );
+            printf( "\nAltura: %d", *ptrAltura );
             pBuffer++;
             }
 
-            free(buffer);
+            free( buffer );
             break;
 
         default:
-            printf("\nEntrada invalida, insira novamente");
+            printf( "\nEntrada invalida, insira novamente" );
             break;
         }
-    } while (op != '0');
+    } while ( op != '0' );
 
     return 0;
 }
