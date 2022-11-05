be15ad75f1cc574ef4742c489e7f3aed713d2c00
RafaelFerrao <rafa5ferrao@gmail.com>
2022-11-04 21:21:43 -0300

    .

--- TrabalhoPratico_Arvores/TrabalhoPraticoArvores_Final.c
+++ TrabalhoPratico_Arvores/TrabalhoPraticoArvores_Final.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Node {
+typedef struct _node {
     int id;
-    struct Node *left;
-    struct Node *right;
+    struct _node *left;
+    struct _node *right;
 } Node;
 
-Node *NewNode(int id) {
-    Node *n = (Node *)malloc(sizeof(Node));
+Node *NewNode( int id ) {
+    Node *n = ( Node * )malloc( sizeof( Node ) );
     n->id = id;
     n->left = NULL;
     n->right = NULL;
@@ -17,7 +17,7 @@ Node *NewNode(int id) {
     return n;
 }
 
-void Insert(Node **root, Node *n) {
+void Insert( Node **root, Node *n ) {
     if( *root == NULL )
         *root = n;
     else {
@@ -26,49 +26,49 @@ void Insert(Node **root, Node *n) {
         else if ( n->id > (*root)->id )
             Insert( &(*root)->right, n );
         else
-            free(n);
+            free( n );
     }
 }
 
-int Height(Node *root) {
+int Height( Node *root ) {
     int left = 0, right = 0;
 
-    if(root == NULL)
+    if( root == NULL )
         return 0;
 
-    left = Height(root->left);
-    right = Height(root->right);
+    left = Height( root->left );
+    right = Height( root->right );
 
-    if(left > right)
+    if( left > right )
         return left + 1;
     else 
         return right + 1;    
 }
 
-int VerifyBF(Node *root) {
-    return Height(root->left) - Height(root->right);
+int VerifyBF( Node *root ) {
+    return Height( root->left ) - Height( root->right );
 }
 
-int IsAvlTree(Node *root) {
+int IsAvlTree( Node *root ) {
 
     int res = 1; // Comecamos supondo que eh arvore avl
     int bf;
 
-    bf = VerifyBF(root);
+    bf = VerifyBF( root );
 
     if(bf > 1 || bf < -1) {
         res = 0;
         return res;
     }
 
-    if(res == 1 && root->left != NULL)
-        res = IsAvlTree(root->left);
+    if( res == 1 && root->left != NULL )
+        res = IsAvlTree( root->left );
     
-    if(res == 1 && root->right != NULL)
-        res = IsAvlTree(root->right);
+    if( res == 1 && root->right != NULL )
+        res = IsAvlTree( root->right );
         
 
-    if(res == 0)
+    if( res == 0 )
         return 0;
 
     return 1;
@@ -84,24 +84,24 @@ int IsLeaf(Node *n) {
 //      10  25
 
 
-void PreOrder(Node *root) {
-    printf("\n%d", root->id);
-    if(root->left != NULL)
-        PreOrder(root->left);
-    if(root->right != NULL)
-        PreOrder(root->right);
+void PreOrder( Node *root ) {
+    printf( "\n%d", root->id );
+    if( root->left != NULL )
+        PreOrder( root->left );
+    if( root->right != NULL )
+        PreOrder( root->right );
 }
 
-void ClearTree(Node *root) {
-    if(root->left != NULL)
-        ClearTree(root->left);
-    if(root->right != NULL)
-        ClearTree(root->right);
-    free(root);
+void ClearTree( Node *root ) {
+    if( root->left != NULL )
+        ClearTree( root->left );
+    if( root->right != NULL )
+        ClearTree( root->right );
+    free( root );
 }
 
 
-int main(int argc, char const *argv[])
+int main( int argc, char const *argv[] )
 {
 
     Node *treeRoot = NULL;
@@ -109,26 +109,26 @@ int main(int argc, char const *argv[])
     int id;
 
     do {
-        printf("\nInsert a number or insert 0 to quit");
-        printf("\nYour choice: ");
-        scanf("%d", &id);
+        printf( "\nInsert a number or insert 0 to quit" );
+        printf( "\nYour choice: " );
+        scanf( "%d", &id );
 
         if( id != 0)
-            Insert(&treeRoot, NewNode(id));
+            Insert( &treeRoot, NewNode( id ) );
 
-    } while (id != 0);
+    } while ( id != 0 );
     
 
-    printf("PreOrder Tree Print: ");
-    PreOrder(treeRoot);
+    printf( "PreOrder Tree Print: " );
+    PreOrder( treeRoot );
 
-    int result = IsAvlTree(treeRoot);
+    int result = IsAvlTree( treeRoot );
     if( result == 1 )
-        printf("\nThis is an AVL tree");
+        printf( "\nThis is an AVL tree" );
     else
-        printf("\nThis is not an AVL tree");
+        printf( "\nThis is not an AVL tree" );
 
-    ClearTree(treeRoot);
+    ClearTree( treeRoot );
 
     return 0;
 }
